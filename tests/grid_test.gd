1d3562efc672195c03f13a9a81088fde97f490bf
Millstreamu <j.winson23@hotmail.com>
2025-10-03 20:54:39 +1000

    Specify type for grid round trip test

--- tests/grid_test.gd
+++ tests/grid_test.gd
@@ -9,5 +9,5 @@ func test_axial_conversion_round_trip() -> void:
             if abs(q + r) > 2:
                 continue
             var position := Coord.axial_to_world(Vector2i(q, r), size)
-            var round_trip := Coord.world_to_axial(position, size)
+            var round_trip: Vector2i = Coord.world_to_axial(position, size)
             assert(round_trip == Vector2i(q, r))
