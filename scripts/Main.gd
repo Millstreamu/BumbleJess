71d5fbc234f2421b86096f5dd079b379bfa1c45f
Millstreamu <j.winson23@hotmail.com>
2025-10-05 19:20:10 +1000

    push to main

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -6,6 +6,6 @@ const HexGrid := preload("res://scripts/grid/HexGrid.gd")
 @onready var hex_grid: HexGrid = $HexGrid
 
 func _ready() -> void:
-    if hex_grid and hex_grid.grid_config:
-        background.color = hex_grid.grid_config.background_color
-    hex_grid.position = get_viewport_rect().size * 0.5
+	if hex_grid and hex_grid.grid_config:
+		background.color = hex_grid.grid_config.background_color
+	hex_grid.position = get_viewport_rect().size * 0.5
--- tests/grid_test.gd
+++ tests/grid_test.gd
@@ -3,11 +3,11 @@ extends RefCounted
 const Coord := preload("res://scripts/core/Coord.gd")
 
 func test_axial_conversion_round_trip() -> void:
-    var size := 32.0
-    for q in range(-2, 3):
-        for r in range(-2, 3):
-            if abs(q + r) > 2:
-                continue
-            var position := Coord.axial_to_world(Vector2i(q, r), size)
-            var round_trip: Vector2i = Coord.world_to_axial(position, size)
-            assert(round_trip == Vector2i(q, r))
+	var size := 32.0
+	for q in range(-2, 3):
+		for r in range(-2, 3):
+			if abs(q + r) > 2:
+				continue
+			var position := Coord.axial_to_world(Vector2i(q, r), size)
+			var round_trip: Vector2i = Coord.world_to_axial(position, size)
+			assert(round_trip == Vector2i(q, r))
