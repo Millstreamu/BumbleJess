4cdf3cf476f4a0620dd5b590b79f2b15991a6ecd
Millstreamu <j.winson23@hotmail.com>
2025-10-05 23:38:58 +1000

    Fix indentation in InputController

--- scripts/input/InputController.gd
+++ scripts/input/InputController.gd
@@ -37,64 +37,64 @@ func _ready() -> void:
 	_palette_state = get_node_or_null(palette_state_path)
 	if not _palette_state:
 		push_warning("InputController could not find PaletteState node at %s" % palette_state_path)
-        if bee_roster_path.is_empty():
-                bee_roster_path = NodePath("../BeeRoster")
-        _bee_roster = get_node_or_null(bee_roster_path)
-        if not _bee_roster:
-                push_warning("InputController could not find BeeRoster node at %s" % bee_roster_path)
-        if assign_bee_picker_path.is_empty():
-                assign_bee_picker_path = NodePath("../AssignBeePicker")
-        _assign_picker = get_node_or_null(assign_bee_picker_path)
-        if not _assign_picker:
-                push_warning("InputController could not find AssignBeePicker node at %s" % assign_bee_picker_path)
+	if bee_roster_path.is_empty():
+		bee_roster_path = NodePath("../BeeRoster")
+	_bee_roster = get_node_or_null(bee_roster_path)
+	if not _bee_roster:
+		push_warning("InputController could not find BeeRoster node at %s" % bee_roster_path)
+	if assign_bee_picker_path.is_empty():
+		assign_bee_picker_path = NodePath("../AssignBeePicker")
+	_assign_picker = get_node_or_null(assign_bee_picker_path)
+	if not _assign_picker:
+		push_warning("InputController could not find AssignBeePicker node at %s" % assign_bee_picker_path)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if not _hex_grid:
 		return
 
-        if event is InputEventKey and event.pressed and not event.echo:
-                if _assign_picker and _assign_picker.is_open():
-                        if _assign_picker.handle_input(event):
-                                get_viewport().set_input_as_handled()
-                                return
-                if event.physical_keycode == KEY_TAB:
-                        if _bee_roster:
-                                if _bee_roster.is_open():
-                                        _bee_roster.close()
-                                else:
-                                        if _palette_state and _palette_state.is_open:
-                                                _palette_state.close()
-                                        _bee_roster.open()
-                        get_viewport().set_input_as_handled()
-                        return
-                if event.physical_keycode == KEY_SPACE:
-                        if not (_bee_roster and _bee_roster.is_open()):
-                                if _try_open_assign_picker():
-                                        get_viewport().set_input_as_handled()
-                                        return
-                if event.physical_keycode == KEY_Z:
-                        if not (_bee_roster and _bee_roster.is_open()):
-                                if _assign_picker and _assign_picker.is_open():
-                                        if _assign_picker.handle_input(event):
-                                                get_viewport().set_input_as_handled()
-                                                return
-                                if _try_unassign_current_cell():
-                                        get_viewport().set_input_as_handled()
-                                        return
-                                if _palette_state:
-                                        if _palette_state.is_open:
-                                                _palette_state.close()
-                                        _has_pending_build = false
-                                        _pending_build_axial = Vector2i.ZERO
-                                        if _palette_state.has_in_hand():
-                                                _palette_state.clear_in_hand()
-                                get_viewport().set_input_as_handled()
-                                return
+	if event is InputEventKey and event.pressed and not event.echo:
+		if _assign_picker and _assign_picker.is_open():
+			if _assign_picker.handle_input(event):
+				get_viewport().set_input_as_handled()
+				return
+		if event.physical_keycode == KEY_TAB:
+			if _bee_roster:
+				if _bee_roster.is_open():
+					_bee_roster.close()
+				else:
+					if _palette_state and _palette_state.is_open:
+						_palette_state.close()
+					_bee_roster.open()
+			get_viewport().set_input_as_handled()
+			return
+		if event.physical_keycode == KEY_SPACE:
+			if not (_bee_roster and _bee_roster.is_open()):
+				if _try_open_assign_picker():
+					get_viewport().set_input_as_handled()
+					return
+		if event.physical_keycode == KEY_Z:
+			if not (_bee_roster and _bee_roster.is_open()):
+				if _assign_picker and _assign_picker.is_open():
+					if _assign_picker.handle_input(event):
+						get_viewport().set_input_as_handled()
+						return
+				if _try_unassign_current_cell():
+					get_viewport().set_input_as_handled()
+					return
+				if _palette_state:
+					if _palette_state.is_open:
+						_palette_state.close()
+					_has_pending_build = false
+					_pending_build_axial = Vector2i.ZERO
+					if _palette_state.has_in_hand():
+						_palette_state.clear_in_hand()
+				get_viewport().set_input_as_handled()
+				return
 
-        if _bee_roster and _bee_roster.is_open():
-                var roster_handled := _bee_roster.handle_input(event)
-                if _bee_roster.consume_assignment_request():
-                        var axial := _hex_grid.get_cursor_axial()
+	if _bee_roster and _bee_roster.is_open():
+		var roster_handled := _bee_roster.handle_input(event)
+		if _bee_roster.consume_assignment_request():
+			var axial := _hex_grid.get_cursor_axial()
 			_bee_roster.try_assign_to_cell(axial)
 			get_viewport().set_input_as_handled()
 			return
@@ -125,17 +125,17 @@ func _unhandled_input(event: InputEvent) -> void:
 			return
 		return
 
-        if _assign_picker and _assign_picker.is_open():
-                if _assign_picker.handle_input(event):
-                        get_viewport().set_input_as_handled()
-                        return
+	if _assign_picker and _assign_picker.is_open():
+		if _assign_picker.handle_input(event):
+			get_viewport().set_input_as_handled()
+			return
 
-        if event.is_action_pressed("ui_accept"):
-                if _bee_roster and _bee_roster.is_assignment_armed():
-                        var target := _hex_grid.get_cursor_axial()
-                        _bee_roster.try_assign_to_cell(target)
-                        get_viewport().set_input_as_handled()
-                        return
+	if event.is_action_pressed("ui_accept"):
+		if _bee_roster and _bee_roster.is_assignment_armed():
+			var target := _hex_grid.get_cursor_axial()
+			_bee_roster.try_assign_to_cell(target)
+			get_viewport().set_input_as_handled()
+			return
 		if _palette_state:
 			_pending_build_axial = _hex_grid.get_cursor_axial()
 			_has_pending_build = true
@@ -144,37 +144,37 @@ func _unhandled_input(event: InputEvent) -> void:
 		return
 
 	for action in MOVE_VECTORS.keys():
-                if event.is_action_pressed(action):
-                        _hex_grid.move_cursor(MOVE_VECTORS[action])
-                        get_viewport().set_input_as_handled()
-                        return
+		if event.is_action_pressed(action):
+			_hex_grid.move_cursor(MOVE_VECTORS[action])
+			get_viewport().set_input_as_handled()
+			return
 
 func _try_open_assign_picker() -> bool:
-        if not _hex_grid or not _assign_picker:
-                return false
-        var axial := _hex_grid.get_cursor_axial()
-        if not _hex_grid.cell_is_eligible_for_bee(axial.x, axial.y):
-                print("[Bees] No slot available at (%d,%d)." % [axial.x, axial.y])
-                return true
-        var cap := _hex_grid.cell_bee_cap(axial.x, axial.y)
-        var used := _hex_grid.cell_bee_count(axial.x, axial.y)
-        if used >= cap:
-                print("[Bees] No slot available at (%d,%d)." % [axial.x, axial.y])
-                return true
-        _assign_picker.open_for_cell(axial)
-        return true
+	if not _hex_grid or not _assign_picker:
+		return false
+	var axial := _hex_grid.get_cursor_axial()
+	if not _hex_grid.cell_is_eligible_for_bee(axial.x, axial.y):
+		print("[Bees] No slot available at (%d,%d)." % [axial.x, axial.y])
+		return true
+	var cap := _hex_grid.cell_bee_cap(axial.x, axial.y)
+	var used := _hex_grid.cell_bee_count(axial.x, axial.y)
+	if used >= cap:
+		print("[Bees] No slot available at (%d,%d)." % [axial.x, axial.y])
+		return true
+	_assign_picker.open_for_cell(axial)
+	return true
 
 func _try_unassign_current_cell() -> bool:
-        if not _hex_grid:
-                return false
-        var axial := _hex_grid.get_cursor_axial()
-        if _hex_grid.cell_bee_count(axial.x, axial.y) <= 0:
-                return false
-        if not Engine.has_singleton("BeeManager"):
-                return false
-        var bee_id := BeeManager.get_last_assigned_bee_for_cell(axial)
-        if bee_id == -1:
-                return false
-        BeeManager.unassign_from_cell(bee_id)
-        print("[Bees] Bee #%d unassigned from (%d,%d)." % [bee_id, axial.x, axial.y])
-        return true
+	if not _hex_grid:
+		return false
+	var axial := _hex_grid.get_cursor_axial()
+	if _hex_grid.cell_bee_count(axial.x, axial.y) <= 0:
+		return false
+	if not Engine.has_singleton("BeeManager"):
+		return false
+	var bee_id := BeeManager.get_last_assigned_bee_for_cell(axial)
+	if bee_id == -1:
+		return false
+	BeeManager.unassign_from_cell(bee_id)
+	print("[Bees] Bee #%d unassigned from (%d,%d)." % [bee_id, axial.x, axial.y])
+	return true
