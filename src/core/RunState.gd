cd2d2cbf70847dfc1a37d3884dcb13d58b5c0c71
Millstreamu <j.winson23@hotmail.com>
2025-10-07 14:45:27 +1000

    Fix RunState static access

--- src/core/RunState.gd
+++ src/core/RunState.gd
@@ -1,28 +1,29 @@
 extends Node
+class_name RunState
 
 const Deck := preload("res://src/systems/Deck.gd")
 
-var seed:int = 0
-var turn:int = 0
-var chosen_variants:Dictionary = {}
-var deck:Array = []
-var draw_index:int = 0
-var overgrowth:Dictionary = {}
-var connected_set:Dictionary = {}
+static var seed: int = 0
+static var turn: int = 0
+static var chosen_variants: Dictionary = {}
+static var deck: Array = []
+static var draw_index: int = 0
+static var overgrowth: Dictionary = {}
+static var connected_set: Dictionary = {}
 
-func start_new_run() -> void:
-				seed = int(Time.get_unix_time_from_system())
-				Config.load_all()
-				chosen_variants = {}
-				deck = []
-				draw_index = 0
-				overgrowth = {}
-				connected_set = {}
-				turn = 0
-				finalize_after_draft()
+static func start_new_run() -> void:
+	seed = int(Time.get_unix_time_from_system())
+	Config.load_all()
+	chosen_variants = {}
+	deck = []
+	draw_index = 0
+	overgrowth = {}
+	connected_set = {}
+	turn = 0
+	RunState.finalize_after_draft()
 
-func finalize_after_draft() -> void:
-	var distribution : Dictionary = {}
+static func finalize_after_draft() -> void:
+	var distribution: Dictionary = {}
 	if Config.deck().has("distribution"):
 		distribution = Config.deck()["distribution"]
 	deck = Deck.build_deck(chosen_variants, distribution, seed)
